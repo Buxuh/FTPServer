3009c274df376013d84596e0a0e35314b841625b
João Quadros <joaoquadros20140396@gmail.com>
2018-11-03 16:43:08 -0100

    Added messages to class Messages and enum class MenuOptions

---
+++ FTPServer/src/org/academiadecodigo/bootcamp/MenuOptions.java
@@ -0,0 +1,25 @@
+package org.academiadecodigo.bootcamp;
+
+public enum MenuOptions {
+
+    VIEW_LIST(1, Message.VIEW_LIST),
+    UPLOAD(2, Message.UPLOAD),
+    DOWNLOAD(3, Message.DOWNLOAD),
+    EXIT(4, Message.BYE_MESSAGE);
+
+    private int options;
+    private String message;
+
+    MenuOptions(int options, String message) {
+        this.options = options;
+        this.message = message;
+    }
+
+    public int getOptions() {
+        return options;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
